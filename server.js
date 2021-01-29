0d4f0f5c5521c50f230e8f386f00682a57c8dbb3
Carolina Salazar <cindy.s.r.28@hotmail.com>
2021-01-29 17:09:18 -0500

    add route for workspace

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const { connect } = require('./src/db');
-const usersRouter = require('./src/routes/user');
+const userRouter = require('./src/routes/user');
+const workspacesRouter = require('./src/routes/workspace');
 
 const port = process.env.PORT || 8000;
 
@@ -12,7 +13,8 @@ connect();
 app.use(express.json());
 app.use(cors());
 
-app.use('/users', usersRouter);
+app.use('/user', userRouter);
+app.use('/workspaces', workspacesRouter);
 
 app.listen(port, () => {
   console.log(`app running at http://localhost:${port}`);
